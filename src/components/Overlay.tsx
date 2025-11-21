07c7c971cd31a0d49b998185a79247167dcb243a
KazumiGIT <varakornm0403@gmail.com>
2025-11-21 15:03:18 +0800

    fix: remove unused imports to fix build

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,5 +1,5 @@
 import { useRef } from 'react';
-import { motion, useScroll, useTransform } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { VideoGallery } from "./VideoGallery";
 import { AnimatedCounter } from "./AnimatedCounter";
 import { CaseStudyCard } from "./CaseStudyCard";
